4defec5674be76d0d181315656e5a0c432ea171c
Konstantin <kpovalaev7@gmail.com>
2022-12-28 19:51:40 +0300

    v

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Container>
+    <ChatWindow>
+      <ChatMessage username="Ivan" time="21.12.2019 05:00:50">
+        Hello, World!!!
+      </ChatMessage>
+    </ChatWindow>
+  </Container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from "./components/Container.vue";
+import ChatWindow from "./components/ChatWindow.vue";
+import ChatMessage from "./components/ChatMessage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Container,
+    ChatWindow,
+    ChatMessage,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
